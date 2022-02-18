a20046587f8a0829b89a866631bf21e397bb596c
Atur-js <78626466+Atur-js@users.noreply.github.com>
2022-02-18 22:14:57 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// Components
+import Landing from './components/Landing';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Landing />
+      </div>
+    </>
+  );
+}
+
+export default App;
